5d7df5cf72c13c501a08625e1f6b131592b210e0
simoxxhunter <kingstonegame@gmail.com>
2024-07-03 16:54:06 +0100

    creation de tableau/model benificiary

---
+++ src/main/java/com/bank/Model/beneficiaireModel.java
@@ -0,0 +1,25 @@
+package com.bank.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Benificiaire")
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class beneficiaireModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long beneficiaireID;
+    private String Name;
+    private String AccountNo;
+    private String BankName;
+
+
+}
