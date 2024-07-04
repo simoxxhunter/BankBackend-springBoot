b8ba636bf15af3368762a8eeba67cacce312244c
simoxxhunter <kingstonegame@gmail.com>
2024-07-04 11:34:21 +0100

    creation de tableau/model transactionModel

---
+++ src/main/java/com/bank/Model/transactionModel.java
@@ -0,0 +1,27 @@
+package com.bank.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity
+@Table(name = "Transactions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class transactionModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long transactionID;
+    private LocalDateTime dateHeure;
+    private double montant;
+    private String type;
+    private String description;
+}
