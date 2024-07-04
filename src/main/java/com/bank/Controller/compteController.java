e23e449ff102bbf4be3a3ef0c1f76587994db51e
simoxxhunter <kingstonegame@gmail.com>
2024-07-04 23:10:23 +0100

    creation de fonction pour creation de compte

--- src/main/java/com/bank/Controller/compteController.java
+++ src/main/java/com/bank/Controller/compteController.java
@@ -1,5 +1,6 @@
 package com.bank.Controller;
 
+import com.bank.Model.compteModel;
 import com.bank.Services.compteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -11,8 +12,19 @@ public class compteController {
     @Autowired
     private compteService compteService;
 
+    @Autowired
+    private com.bank.Dao.compteDao compteDao;
+
+    @PostMapping("/{createNewAccount}")
+    public void createAccount(@RequestBody compteModel compte) {
+        compteDao.save(compte);
+        System.out.println("compte cree avec succee");
+
+    }
+
     @GetMapping("/{compteId}")
     public double getCompteBalance(@PathVariable int compteId) {
         return compteService.getCompteBalance(compteId);
     }
+
 }
--- src/main/java/com/bank/Dao/compteDao.java
+++ src/main/java/com/bank/Dao/compteDao.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface compteDao extends JpaRepository<compteModel, Integer> {
+    public compteModel save(compteModel compte);
+
 }
