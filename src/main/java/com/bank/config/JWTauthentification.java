2aa96cd1911671fbe3cc40626e8f2f93d6228438
simoxxhunter <kingstonegame@gmail.com>
2024-07-09 20:54:06 +0100

    configuration du jwt

--- src/main/java/com/bank/config/JWTauthentification.java
+++ src/main/java/com/bank/config/JWTauthentification.java
@@ -1,5 +1,6 @@
 package com.bank.config;
 
+import com.bank.Services.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,9 +14,20 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class JWTauthentification extends OncePerRequestFilter {
+
+    final JwtService jwtService;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt ;
+        if (authHeader != null || authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+
+        }
+        jwt = authHeader.substring(7);
+        userName = jwtService.extractUsername(jwt);
 
     }
 }
