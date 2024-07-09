040136c413afd3aaa42810f046284e683bc76622
simoxxhunter <kingstonegame@gmail.com>
2024-07-09 21:44:45 +0100

    configuration de cle secret

--- src/main/java/com/bank/Services/JwtService.java
+++ src/main/java/com/bank/Services/JwtService.java
@@ -1,12 +1,31 @@
 package com.bank.Services;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
+    private static final String SECRET_KEY = "54 68 65 20 71 75 69 63 6b 20 62 72 6f 77 6e 20 66 6f 78 20 6a 75 6d 70 73 20 6f 76 65 72 20 31 33 20 6c 61 7a 79 20 64 6f 67 73 2e";
 
     public String extractUsername(String token) {
         return null;
+    }
+    private Claims extractClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+    }
+
+    private byte[] getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes).getEncoded();
     }
 
 }
