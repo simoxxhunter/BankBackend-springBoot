9cde4a0c115ab2d91f3a3394d9706d51244d05c4
simoxxhunter <kingstonegame@gmail.com>
2024-07-11 11:26:10 +0100

    creation de security config

--- src/main/java/com/bank/config/SecurityConfig.java
+++ src/main/java/com/bank/config/SecurityConfig.java
@@ -1,13 +1,18 @@
 package com.bank.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import java.beans.JavaBean;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 
 public class SecurityConfig {
+
+    @Bean
 
 }
