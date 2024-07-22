d6cf44829e96f0347ef157da4ee861cc2ff5211c
simoxxhunter <kingstonegame@gmail.com>
2024-07-22 22:35:26 +0100

    implimentation de method logout et resolution d'erreur de login

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { Subscription } from 'rxjs';
 import { MatIconModule } from '@angular/material/icon';
@@ -7,6 +7,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatListModule } from '@angular/material/list';
+import { AuthService } from './services/Account/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +15,18 @@ import { MatListModule } from '@angular/material/list';
   styleUrls: ['./app.component.css'],
   
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'Ebank';
+
+
+  constructor(public authService: AuthService) {}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+
+  logout() {
+    this.authService.logout();
+  }
 }
